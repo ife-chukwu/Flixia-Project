a287b0a02d0ed7e2e241c857c3f078e49dca923d
ifechukwu <dopeskiez399@gmail.com>
2022-10-03 22:27:10 +0100

    mobile responsiveness

--- src/Components/HeroItems.js
+++ src/Components/HeroItems.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { BsFacebook } from "react-icons/bs";
+import { AiFillGoogleCircle } from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
+import { AiFillTwitterCircle } from "react-icons/ai";
 
 const HeroItems = () => {
   return (
-    <div className="text-white leading-[5rem]  py-[2rem]  backdrop-blur-[3px]  relative">
+    <div className="md:text-white leading-[5rem] bg-black/10 h-[100vh] backdrop-blur-[6px]   w-screen">
       <div
         className="jumbo-text "
         data-aos="fade-up"
@@ -11,27 +15,47 @@ const HeroItems = () => {
       >
         <h1
           className=" jumbotron md:text-shadow text-[4rem] font-extrabold 
-      text-orange-400  backdrop-blur-[5px]
-       pt-2 pb-11  px-12 "
+      text-orange-400 
+       pt-2  px-12 mt-11"
         >
           {" "}
           FLIXIA
-          <span className=" Jumbotron-span md:text-[4rem] md:text-white block font-extrabold   leading-6">
+          <span className=" Jumbotron-span md:text-[4rem] md:text-white block font-extrabold   leading-[4rem]">
             GLOBAL{" "}
           </span>{" "}
           STREAMS
         </h1>
       </div>
-      <div className="  backdrop-blur-[5px]  px-2 py-10">
-        <h1 className="text-shadow text-white flex   text-[2rem] font-bold pl-[55px] leading-9">
+      <div className="mt-5">
+        <h1 className=" query md:text-shadow text-white flex   text-[2rem] font-bold pl-[55px] leading-9">
           Who Are We?
         </h1>
-        <p className="leading-[18px]  text-[14px] pl-[55px] font-thin">
+        <p className="query-asw  md:leading-[18px]  md:text-[14px] pl-[55px] md:font-thin">
           We are known as Flixia Streaming Music platform round the globe,
           <br /> we make absolute sure to bring the latest hit to our
           patronizers.
           <br /> We are rated highly among the rest of other Streaming platforms
         </p>
+      </div>
+
+      <div className="footer md:bg-black flex items-end w-full justify-center hover:bg-black  transition duration-700 text-orange-400 md:first-letter:mt-[13%] md:h-[80px] leading-3">
+        <div className="pt-3 text-center">
+          <footer className="footer-icon  md:flex justify-center items-center gap-3 md:text-lg  cursor-pointer md:pb-2">
+            <BsFacebook className="icon md:hover:text-white md:transition md:duration-700 text-lg" />
+            <AiFillGoogleCircle className="icon md:hover:text-white md:transition md:duration-700 text-lg" />
+            <AiFillInstagram className=" icon md:hover:text-white md:transition md:duration-700 text-lg" />
+            <AiFillTwitterCircle className=" icon placeholder:md:hover:text-white md:transition md:duration-700 text-lg" />
+          </footer>
+          <p className="footer-text md:tracking-wide md:pb-2">
+            We are music{" "}
+            <span className="text-white hover:text-orange  transition duration-700">
+              Music is us
+            </span>
+          </p>
+          <p className=" footer-text md:pb-3">
+            Copyright @2021 FLIXIA
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/Components/HeroPage.js
+++ src/Components/HeroPage.js
@@ -1,12 +1,8 @@
 import React from "react";
 import { HiOutlineSearch } from "react-icons/hi";
-import { BsFacebook } from "react-icons/bs";
-import { AiFillGoogleCircle } from "react-icons/ai";
-import { AiFillInstagram } from "react-icons/ai";
-import { AiFillTwitterCircle } from "react-icons/ai";
-import { useState } from "react";
 import NavbarMd from "./NavbarMd";
 import HeroItems from "./HeroItems";
+import { useState } from "react";
 
 function Hero() {
   const [inputState, setInputState] = useState("");
@@ -43,8 +39,8 @@ function Hero() {
   };
   return (
     <div
-      className=" xs:bg-hero-image bg-hero-image bg-no-repeat bg-cover h-[100%] bg-center w-full 
-                     md:bg-hero-image md:bg-no-repeat md:bg-cover md:bg-bottom md:w-full md:h-[100vh]"
+      className=" xs:bg-hero-image bg-hero-image bg-no-repeat bg-cover h-[100vh] bg-center w-full 
+                     md:bg-hero-image md:bg-no-repeat md:bg-cover md:bg-bottom md:w-full md:h-[100%]"
     >
       <div
         className="mobile-nav
@@ -54,7 +50,7 @@ function Hero() {
           <img
             src="Flixialogo.png"
             alt="Flixia"
-            className="logo md:w-[190px] md:h-15 px-3 items-center md:mt-3  shadow-4xl"
+            className="logo md:w-[190px] md:h-10 px-3 items-center md:mt-3  shadow-4xl"
             data-aos="zoom-in-left"
             data-aos-duration="3000"
           />
@@ -113,23 +109,23 @@ function Hero() {
         </ul>
       </div>
       <div className=" pl-0  md:flex flex-col md:pl-[60%] absolute  z-30">
-      <button
-          className="button md:py-6 flex md:text-[16px] h-8 px-11 md:ml-[50px] md:w-40 md:flex border-2xl bg-orange-400 hover:bg-orange-500 hover:text-white transition duration-400 text-white pl-11 py-2 mr-11
+        <button
+          className="button md:py-2 flex md:text-[16px]  px-15 md:ml-[50px] md:w-40 md:flex border-2xl bg-orange-400 hover:bg-orange-500 hover:text-white transition duration-400 text-white pl-11 py-2 mr-11
           rounded-xl items-center text-base  mt-[50px] ml-[10px] "
           onClick={openFormHandler}
         >
-          {openForm ? "Close form" : "Login here"}
+          {openForm ? "cancel" : "Login here"}
         </button>
         <div>
           {openForm && (
             <form
               onSubmit={buttonHandler}
-              className=" mobile-form bg-black/50"
+              className="flex flex-col  mt-[14px] 1-[100%] px-[46px] bg-black/80 md:flex md:flex-col md:mt-2 md:bg-black/70 h-[60vh] pt-16 pl-11 md:px-[40px] md:rounded-2xl "
               data-aos="fade-down"
               data-aos-easing="linear"
               data-aos-duration="500"
             >
-              <fieldset className="text-white border-2xl border-white tracking-wide mt-[-50px]">
+              <fieldset className="text-white w-[120px] md:text-white border-2xl border-white tracking-wide mt-[-50px]">
                 Please enter your login details{" "}
               </fieldset>
               <input
@@ -139,7 +135,7 @@ function Hero() {
                 placeholder="Username"
                 onChange={enteredUserNameInput}
                 value={userNameInputState}
-                className=" flex mb-[-15px] py-2 w-[400px]  outline-none rounded-2xl text-lg mt-[40px] p-4"
+                className=" flex w-[300px] py-1 text-sm rounded-md md:flex mb-[-15px] md:py-2 md:w-[400px]  outline-none md:rounded-2xl md:text-lg mt-[40px] p-4"
               />
               <input
                 required
@@ -148,7 +144,7 @@ function Hero() {
                 onChange={enteredUserEmailInput}
                 value={userEmailInputState}
                 placeholder="Email"
-                className=" flex mb-6 py-2 w-[400px]  outline-none rounded-2xl text-lg mt-[40px] p-4"
+                className="flex w-[300px] py-1 text-sm rounded-md md: md:flex mb-6 md:py-2 md:w-[400px]  outline-none md:rounded-2xl md:text-lg mt-[40px] p-4"
               />
               <input
                 required
@@ -157,10 +153,11 @@ function Hero() {
                 onChange={enteredUserPasswordInput}
                 value={userPasswordInputState}
                 placeholder="Password"
-                className=" w-[400px] py-2 outline-none  rounded-2xl text-lg p-4"
+                className="flex w-[300px] py-1 text-sm rounded-md  md:w-[400px] md:py-2 outline-none  md:rounded-2xl md:text-lg p-4"
               />
 
-              <button className="lg:flex border-2xl bg-orange-400  hover:bg-orange-500 hover:text-white  transition duration-400 text-white pl-[50px] py-3   rounded-xl items-center text-base ml-[120px] w-40 mt-11 shadow-lg shadow-black">
+              <button className="submit-btn flex 
+               md:flex border-2xl bg-orange-400  hover:bg-orange-500 hover:text-white  transition duration-400 text-white md:pl-[50px] md:py-3   md:rounded-xl text-center text-base md:ml-[120px] md:w-40 md:mt-11 shadow-lg shadow-black">
                 Submit
               </button>
             </form>
@@ -170,27 +167,6 @@ function Hero() {
       <div className="flex">
         <HeroItems className="relative" />
         <NavbarMd />
-      </div>
-      <div className="bg-black/50 absolute mt-[25px] w-full  hover:bg-black  transition duration-700 text-orange-400">
-        <div className="pt-3">
-          <footer className="flex justify-center items-center gap-3 text-lg  cursor-pointer">
-            <BsFacebook className="hover:text-white transition duration-700 text-lg" />
-            <AiFillGoogleCircle className="hover:text-white transition duration-700 text-lg" />
-            <AiFillInstagram className="hover:text-white transition duration-700 text-lg" />
-            <AiFillTwitterCircle className="hover:text-white transition duration-700 text-lg" />
-          </footer>
-          <div className="flex flex-col justify-center mt-1 ml-[45%]">
-            <p className="tracking-wide">
-              We are music{" "}
-              <span className="text-white hover:text-orange  transition duration-700">
-                Music is us
-              </span>
-            </p>
-            <p className="tracking-wide text-orange-400 hover:text-white transition duration-700 pb-3">
-              Copyright @2021 FLIXIA
-            </p>
-          </div>
-        </div>
       </div>
     </div>
   );
